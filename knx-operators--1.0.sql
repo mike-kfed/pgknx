3fa9528b366fd49fe671836eb84753cea1c012bd
Michael Kefeder <m.kefeder@gmail.com>
2019-03-05 12:07:26 +0100

    initial idea for operators

--- knx-operators--1.0.sql
+++ knx-operators--1.0.sql
@@ -1,32 +1,69 @@
+--
+-- Operator functions:
+--
+---------------------------------------------------
+CREATE FUNCTION knx_ga3_lt(knx_group_address3, knx_group_address3)
+	RETURNS boolean
+	AS 'int4lt'
+	LANGUAGE 'internal'
+	IMMUTABLE STRICT
+	PARALLEL SAFE;
+CREATE FUNCTION knx_ga3_le(knx_group_address3, knx_group_address3)
+	RETURNS boolean
+	AS 'int4le'
+	LANGUAGE 'internal'
+	IMMUTABLE STRICT
+	PARALLEL SAFE;
+CREATE FUNCTION knx_ga3_eq(knx_group_address3, knx_group_address3)
+	RETURNS boolean
+	AS 'int4eq'
+	LANGUAGE 'internal'
+	IMMUTABLE STRICT
+	PARALLEL SAFE;
+CREATE FUNCTION knx_ga3_ge(knx_group_address3, knx_group_address3)
+	RETURNS boolean
+	AS 'int4ge'
+	LANGUAGE 'internal'
+	IMMUTABLE STRICT
+	PARALLEL SAFE;
+CREATE FUNCTION knx_ga3_gt(knx_group_address3, knx_group_address3)
+	RETURNS boolean
+	AS 'int4gt'
+	LANGUAGE 'internal'
+	IMMUTABLE STRICT
+	PARALLEL SAFE;
+CREATE FUNCTION knx_ga3_ne(knx_group_address3, knx_group_address3)
+	RETURNS boolean
+	AS 'int4ne'
+	LANGUAGE 'internal'
+	IMMUTABLE STRICT
+	PARALLEL SAFE;
+
 
 --
 -- Now the operators:
 --
-/*
---
--- GTIN-14 operators:
---
 ---------------------------------------------------
 CREATE OPERATOR < (
-	PROCEDURE = isnlt,
-	LEFTARG = gtin14,
-	RIGHTARG = gtin14,
+	PROCEDURE = knx_ga3_lt,
+	LEFTARG = knx_group_address3,
+	RIGHTARG = knx_group_address3,
 	COMMUTATOR = >,
 	NEGATOR = >=,
 	RESTRICT = scalarltsel,
 	JOIN = scalarltjoinsel);
 CREATE OPERATOR <= (
-	PROCEDURE = isnle,
-	LEFTARG = gtin14,
-	RIGHTARG = gtin14,
+	PROCEDURE = knx_ga3_le,
+	LEFTARG = knx_group_address3,
+	RIGHTARG = knx_group_address3,
 	COMMUTATOR = >=,
 	NEGATOR = >,
 	RESTRICT = scalarltsel,
 	JOIN = scalarltjoinsel);
 CREATE OPERATOR = (
-	PROCEDURE = isneq,
-	LEFTARG = gtin14,
-	RIGHTARG = gtin14,
+	PROCEDURE = knx_ga3_eq,
+	LEFTARG = knx_group_address3,
+	RIGHTARG = knx_group_address3,
 	COMMUTATOR = =,
 	NEGATOR = <>,
 	RESTRICT = eqsel,
@@ -34,25 +71,25 @@ CREATE OPERATOR = (
 	MERGES,
 	HASHES);
 CREATE OPERATOR >= (
-	PROCEDURE = isnge,
-	LEFTARG = gtin14,
-	RIGHTARG = gtin14,
+	PROCEDURE = knx_ga3_ge,
+	LEFTARG = knx_group_address3,
+	RIGHTARG = knx_group_address3,
 	COMMUTATOR = <=,
 	NEGATOR = <,
 	RESTRICT = scalargtsel,
 	JOIN = scalargtjoinsel );
 CREATE OPERATOR > (
-	PROCEDURE = isngt,
-	LEFTARG = gtin14,
-	RIGHTARG = gtin14,
+	PROCEDURE = knx_ga3_gt,
+	LEFTARG = knx_group_address3,
+	RIGHTARG = knx_group_address3,
 	COMMUTATOR = <,
 	NEGATOR = <=,
 	RESTRICT = scalargtsel,
 	JOIN = scalargtjoinsel );
 CREATE OPERATOR <> (
-	PROCEDURE = isnne,
-	LEFTARG = gtin14,
-	RIGHTARG = gtin14,
+	PROCEDURE = knx_ga3_ne,
+	LEFTARG = knx_group_address3,
+	RIGHTARG = knx_group_address3,
 	COMMUTATOR = <>,
 	NEGATOR = =,
 	RESTRICT = neqsel,
@@ -63,38 +100,36 @@ CREATE OPERATOR <> (
 --
 ---------------------------------------------------
 
-CREATE OPERATOR FAMILY isn_ops USING btree;
-CREATE OPERATOR FAMILY isn_ops USING hash;
+CREATE OPERATOR FAMILY knx_group_address_ops USING btree;
+CREATE OPERATOR FAMILY knx_group_address_ops USING hash;
 
 --
 -- Operator classes:
 --
 ---------------------------------------------------
--- GTIN-14:
-CREATE FUNCTION btgtin14cmp(gtin14, gtin14)
+CREATE FUNCTION btknx_group_address3cmp(knx_group_address3, knx_group_address3)
 	RETURNS int4
-	AS 'btint8cmp'
+	AS 'btint4cmp'
 	LANGUAGE 'internal'
 	IMMUTABLE STRICT
 	PARALLEL SAFE;
 
-CREATE OPERATOR CLASS gtin14_ops DEFAULT
-	FOR TYPE gtin14 USING btree FAMILY isn_ops AS
+CREATE OPERATOR CLASS knx_group_address3_ops DEFAULT
+	FOR TYPE knx_group_address3 USING btree FAMILY knx_group_address_ops AS
 	OPERATOR 1  <,
 	OPERATOR 2  <=,
 	OPERATOR 3  =,
 	OPERATOR 4  >=,
 	OPERATOR 5  >,
-	FUNCTION 1  btgtin14cmp(gtin14, gtin14);
+	FUNCTION 1  btknx_group_address3cmp(knx_group_address3, knx_group_address3);
 
-CREATE FUNCTION hashgtin14(gtin14)
+CREATE FUNCTION hashknx_group_address3(knx_group_address3)
 	RETURNS int4
-	AS 'hashint8'
+	AS 'hashint4'
 	LANGUAGE 'internal' IMMUTABLE STRICT
 	PARALLEL SAFE;
 
-CREATE OPERATOR CLASS gtin14_ops DEFAULT
-	FOR TYPE gtin14 USING hash FAMILY isn_ops AS
+CREATE OPERATOR CLASS knx_group_address3_ops DEFAULT
+	FOR TYPE knx_group_address3 USING hash FAMILY knx_group_address_ops AS
 	OPERATOR 1  =,
-	FUNCTION 1  hashgtin14(gtin14);
-*/
+	FUNCTION 1  hashknx_group_address3(knx_group_address3);
